3cdb30d3bf60d43ca16213527006a47e3c8a2f9c
TulioContramaestre <contramaestre.tulio@gmail.com>
2026-05-02 03:39:40 -0400

    fix: change the cards to become an overaly instead of two seperate sections for image and label to allow for better alignment and ui design

--- eucf-website/src/components/OfficerCard.tsx
+++ eucf-website/src/components/OfficerCard.tsx
@@ -12,22 +12,18 @@ export default function OfficerCard({ name, position, image, isActive, onTap }: 
   return (
     <div
       onClick={onTap}
-      className={`shrink-0 transition-transform duration-300 hover:scale-105 border-2
+      className={`shrink-0 relative w-65 sm:w-85 md:w-100 aspect-3/4 bg-amber-400 transition-transform duration-300 hover:scale-105 border-2
                   hover:border-black rounded-lg overflow-hidden cursor-pointer
                   ${isActive ? "scale-105 border-black" : "border-transparent"}`}
     >
-      <div className="overflow-hidden">
-        <div className="relative w-65 sm:w-85 md:w-100 aspect-3/4 bg-amber-400">
-          <Image
-            src={image}
-            alt={`${name}, ${position} of EUCF`}
-            fill
-            loading="lazy"
-            className="object-cover"
-          />
-        </div>
-      </div>
-      <div className="bg-black text-white p-3">
+      <Image
+        src={image}
+        alt={`${name}, ${position} of EUCF`}
+        fill
+        loading="lazy"
+        className="object-cover"
+      />
+      <div className="absolute bottom-0 left-0 right-0 bg-black/85 text-white p-3">
         <p className="text-sm font-bold uppercase tracking-wide text-[#B49758]">
           {position}
         </p>
